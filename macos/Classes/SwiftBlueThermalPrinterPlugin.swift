48bb692952a504950146f7bb7a60a578673a7e85
tran.huy <thanhhuy0611@gmail.com>
2022-07-01 09:38:02 +0700

    fix: can not run app on MacOS

--- macos/Classes/SwiftBlueThermalPrinterPlugin.swift
+++ macos/Classes/SwiftBlueThermalPrinterPlugin.swift
@@ -1,14 +1,15 @@
-import Flutter
-import UIKit
+import FlutterMacOS
+import AppKit
 
-public class SwiftDragoBluePrinterPlugin: NSObject, FlutterPlugin {
+public class DragoBluePrinterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "drago_blue_printer", binaryMessenger: registrar.messenger())
-    let instance = SwiftDragoBluePrinterPlugin()
+    let channel = FlutterMethodChannel(name: "drago_blue_printer", binaryMessenger: registrar.messenger)
+    let instance = DragoBluePrinterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    // result("iOS " + UIDevice.current.systemVersion)
+    result(NSNumber(value: 0))
   }
 }
