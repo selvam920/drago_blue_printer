af90806f85b4569dadb637fceb82ed4d7f90b365
selvam920 <skssoftware@live.com>
2023-06-03 16:35:29 +0530

    updated pub

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\SW\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\sksso\Documents\GitHub\drago_blue_printer\example"
+export "FLUTTER_ROOT=D:\Software\flutter_windows_3.11.0-0.1.pre-beta\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\drago_blue_printer\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
